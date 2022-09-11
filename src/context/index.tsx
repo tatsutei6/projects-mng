af9a1a52ac34f7f97f8e0e47b54b4dd63bcc12df
tatsutei6 <tatsutei6@gmail.com>
2022-09-11 17:52:42 +0900

    Updates

--- src/context/index.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react'
-import { AuthProvider } from './AuthContext'
-
-export const AppProviders = ({ children }: { children: ReactNode }) => {
-  return <AuthProvider>{children}</AuthProvider>
-}
